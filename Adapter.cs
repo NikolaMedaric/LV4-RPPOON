b2657196263b0b71a3604f5eb175f4ef35a736da
NikolaMedaric <63855981+NikolaMedaric@users.noreply.github.com>
2020-06-05 18:26:58 +0200

    Add files via upload

---
+++ Adapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV4
+{
+    class Adapter : IAnalytics
+    {
+        private Analyzer3rdParty analyticsService;
+        public Adapter(Analyzer3rdParty service)
+        {
+            this.analyticsService = service;
+        }
+
+        private double[][] ConvertData(Dataset dataset)
+        {
+            //implementation missing!
+            double[][] data = this.dataset.GetData();
+            return this.analyticsService.ConvertData(data);
+        }
+
+        public double[] CalculateAveragePerColumn(Dataset dataset)
+        {
+            double[][] data = this.ConvertData(dataset);
+            return this.analyticsService.PerColumnAverage(data);
+        }
+
+        public double[] CalculateAveragePerRow(Dataset dataset)
+        {
+            double[][] data = this.ConvertData(dataset);
+            return this.analyticsService.PerRowAverage(data);
+        }
+    }
+}
---
+++ Analyzer3rdParty.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV4
+{
+    class Analyzer3rdParty
+    {
+        public double[] PerRowAverage(double[][] data)
+        {
+            int rowCount = data.Length;
+            double[] results = new double[rowCount];
+            for (int i = 0; i < rowCount; i++)
+            {
+                results[i] = data[i].Average();
+            }
+            return results;
+        }
+        public double[] PerColumnAverage(double[][] data)
+        { //implementation missing!
+            int rowCount = data.Length(0);
+            int columnCount = data.Length(1);
+            double[] results = new double[columnCount];
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    results[j] = data[i].Average();
+                }
+            }
+            return results;
+        }
+    }
+}
